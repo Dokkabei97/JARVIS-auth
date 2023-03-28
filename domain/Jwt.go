fe7b5cad844dc42c3fcd4757d12b5d1d3792b021
Dokkabei97 <wkdrn970@naver.com>
2023-03-28 22:33:34 +0900

    feat: jwt struct

--- domain/Jwt.go
+++ domain/Jwt.go
@@ -3,20 +3,20 @@ package domain
 import "time"
 
 type AccessToken struct {
-	UserId  int64
-	Email   string
-	IsAdmin bool
-	IsBlock bool
+	UserId  int64  `json:"userId"`
+	Email   string `json:"email"`
+	IsAdmin bool   `json:"isAdmin"`
+	IsBlock bool   `json:"isBlock"`
 }
 
 type RefreshToken struct {
-	UserId int64
+	UserId int64 `json:"userId"`
 }
 
 type Token struct {
-	Id           int64
-	userId       int64
-	AccessToken  AccessToken
-	RefreshToken RefreshToken
-	CreatedAt    time.Time
+	Id           int64        `json:"id"`
+	UserId       int64        `json:"userId"`
+	AccessToken  AccessToken  `json:"accessToken"`
+	RefreshToken RefreshToken `json:"refreshToken"`
+	CreatedAt    time.Time    `json:"createdAt"`
 }
